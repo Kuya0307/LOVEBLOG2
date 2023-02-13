600fdc74f160c8eeb6c97f985a4c0483257f5d94
pati <y.kumagai.sys22@morijyobi.ac.jp>
2023-02-13 10:04:33 +0900

    [add]投稿ページの作成

--- php/post.php
+++ php/post.php
@@ -3,23 +3,46 @@ require_once 'db_connect.php'
 ?>
 <html>
   <br>
+  <form action="post2.php" method="post">
 <label for="story">タイトル</label>
-<input type="text"><br>
-<textarea id="story" name="story"
-          rows="30" cols="100">
-投稿内容を入力してください。
+<input type="text"name="title"><br>
+<textarea id="story" name="content"
+          rows="30" cols="100" placeholder="投稿内容を入力してください。">
 </textarea>
-<img src="../img/gomi.png">
+<button type="reset">
+<img src="../img/gomi.png" width="100px" height="100px">
+</button>
 <p>投稿
-      <input type="radio"checked>公開
-      <input type="radio">非公開
-    </p>
+      <input type="radio"checked name="flag"value="1">公開
+      <input type="radio" name="flag"value="0">非公開
+</p>
+
     <button class="favorite styled"
-        type="button">
-    投稿
+        type="submit">  投稿
 </button>
-<button class="favorite styled"
-        type="button">
-    キャンセル
+</form>
+<button id="btn">キャンセル</button>
+
+
+
+<script>
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function() {
+    var result = window.confirm('ボタンをクリック！');
+    
+    if( result ) {
+        //OKを押して遷移させる
+        window.location.href = '../php/wia.php';
+    }else {
+        //キャンセルを押してリダイレクトさせる
+        window.location.href = '../php/post.php';
+    }
+})
+</script>
+
+</script>
+  
 </button>
+
 </html>
