a0b9d42478b7f4e17f8db325a502d4db1400c4ba
kuya <k.oda.sys22@morijyobi.ac.jp>
2023-03-13 10:00:22 +0900

    cssを編集

--- php/update.php
+++ php/update.php
@@ -8,11 +8,17 @@ $title = $_POST['title'];
 $content = $_POST['content'];
 $title = htmlspecialchars($title,ENT_QUOTES,"UTF-8");
 $content = htmlspecialchars($content,ENT_QUOTES,"UTF-8");
-$sql  = "UPDATE report SET title = :title, content = :content WHERE id = :id";
-$stm = $pdo->prepare($sql);
-$stm->bindValue(':title',$title,PDO::PARAM_STR);
-$stm->bindValue(':content',$content,PDO::PARAM_STR);
-$stm->bindValue(':id',$ID,PDO::PARAM_INT);
-$stm->execute();
-$upd = $stm->fetch(PDO::FETCH_ASSOC);
-header('Location:table.php');
+$url = "upform.php?ID=".$ID;
+if(empty($title) || empty($content)){
+    header('Location:'.$url);
+    $_SESSION['null_error'] = "タイトルまたは内容が入力されていません";
+}elseif(isset($title) && isset($content)){
+    $sql  = "UPDATE report SET title = :title, content = :content WHERE id = :id";
+    $stm = $pdo->prepare($sql);
+    $stm->bindValue(':title',$title,PDO::PARAM_STR);
+    $stm->bindValue(':content',$content,PDO::PARAM_STR);
+    $stm->bindValue(':id',$ID,PDO::PARAM_INT);
+    $stm->execute();
+    $upd = $stm->fetch(PDO::FETCH_ASSOC);
+    header('Location:table.php');
+}
